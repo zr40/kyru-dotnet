774195c0847889a8d3329d82f5e20d37d42e18ab
fiona <shixiangyunfiona@hotmail.com>
2012-10-10 21:09:44 +0200

    updated chunk and put the right code in KFile

---
+++ Kyru/Core/Chunk.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kyru
+{
+    class Chunk
+    {
+        public Byte[] Data;
+        public Byte[] key;
+        public DateTime timestamp;
+
+        public Chunk()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Kyru/Core/KFile.cs
+++ Kyru/Core/KFile.cs
@@ -5,7 +5,36 @@ using System.Text;
 
 namespace Kyru.Core
 {
-	internal class KFile
-	{
-	}
+    class KFile
+    {
+        Byte[] Data;
+        public string Name;
+        public string Extension;
+
+        public KFile(List<Chunk> chunks)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Chunk> Split()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rename(string newName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Load(string Name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string Name)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
 }
