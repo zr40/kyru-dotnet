e68e9521e10fa55192a6f6781026648711c8ce27
Matthijs van der Vleuten <zr40.nl@gmail.com>
2012-10-10 20:08:44 +0200

    Add App (group assignment)

---
+++ Kyru/App.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Kyru.Network;
+
+namespace Kyru
+{
+	class App
+	{
+
+		internal void Start()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal int FindCopyCount(KademliaId objectId)
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void GetAndDecryptAndStoreFile(string localPath /* TODO parameters: Session, File, Node, NetworkManager */)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
