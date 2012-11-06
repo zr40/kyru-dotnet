e0a67eb8fd3f29d95299163aed850af5ee6ee2ec
Jasmijn Zoun <jasmijn@zoun.eu>
2012-11-06 22:24:56 +0100

    implemented Crypto.sign()
    Also splitted key generation methods and added docstrings to them

--- Kyru/Core/Crypto.cs
+++ Kyru/Core/Crypto.cs
@@ -9,7 +9,24 @@ namespace Kyru.Core
 		private static readonly SHA1CryptoServiceProvider hash = new SHA1CryptoServiceProvider();
 
 		internal const int RsaPublicKeySize = 2048;
-		internal static void GenerateKeyPair(string username, string password, out byte[] privateKey, out byte[] publicKey)
+
+		/// <summary>
+		/// Generates a private key from a username and password
+		/// </summary>
+		/// <param name="username">Username</param>
+		/// <param name="password">Password</param>
+		/// <returns>The private key</returns>
+		internal static byte[] GeneratePrivateKey(string username, string password)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Extracts the public key from private key data
+		/// </summary>
+		/// <param name="privateKey">The private key</param>
+		/// <returns>The public key</returns>
+		internal static byte[] ExtractPublicKey(byte[] privateKey)
 		{
 			throw new NotImplementedException();
 		}
@@ -33,14 +50,28 @@ namespace Kyru.Core
 		{
 			throw new NotImplementedException();
 		}
+
+		/// <summary>
+		/// Returns a Kyru spec hash of the data provided
+		/// </summary>
+		/// <param name="data">Data to hash</param>
+		/// <returns>The Hash of the data</returns>
 		internal static byte[] Hash(byte[] data)
 		{
 			return hash.ComputeHash(data);
 		}
 
+		/// <summary>
+		/// Generates a signature for the data
+		/// </summary>
+		/// <param name="data">Data to sign</param>
+		/// <param name="privateKey">Key to sign the data with</param>
+		/// <returns>The signature of the hashed data</returns>
 		internal static byte[] Sign(byte[] data, byte[] privateKey)
 		{
-			throw new NotImplementedException();
+			var rsa = new RSACryptoServiceProvider();
+			rsa.ImportCspBlob(privateKey);
+			return rsa.Decrypt(Hash(data), true);
 		}
 
 		/// <summary>
