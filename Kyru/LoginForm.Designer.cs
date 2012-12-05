500549d61f57c767bda1523e1592c84cc2deb0a3
Jasmijn Zoun <jasmijn@zoun.eu>
2012-12-05 17:07:36 +0100

    Set login form's acceptbutton

--- Kyru/LoginForm.Designer.cs
+++ Kyru/LoginForm.Designer.cs
@@ -28,70 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-			  this.label1 = new System.Windows.Forms.Label();
-			  this.label2 = new System.Windows.Forms.Label();
-			  this.txtUsername = new System.Windows.Forms.TextBox();
-			  this.btnLogin = new System.Windows.Forms.Button();
-			  this.txtPassword = new System.Windows.Forms.TextBox();
-			  this.SuspendLayout();
-			  // 
-			  // label1
-			  // 
-			  this.label1.AutoSize = true;
-			  this.label1.Location = new System.Drawing.Point(12, 16);
-			  this.label1.Name = "label1";
-			  this.label1.Size = new System.Drawing.Size(58, 13);
-			  this.label1.TabIndex = 0;
-			  this.label1.Text = "Username:";
-			  // 
-			  // label2
-			  // 
-			  this.label2.AutoSize = true;
-			  this.label2.Location = new System.Drawing.Point(12, 46);
-			  this.label2.Name = "label2";
-			  this.label2.Size = new System.Drawing.Size(56, 13);
-			  this.label2.TabIndex = 1;
-			  this.label2.Text = "Password:";
-			  // 
-			  // txtUsername
-			  // 
-			  this.txtUsername.Location = new System.Drawing.Point(77, 13);
-			  this.txtUsername.Name = "txtUsername";
-			  this.txtUsername.Size = new System.Drawing.Size(149, 20);
-			  this.txtUsername.TabIndex = 2;
-			  // 
-			  // btnLogin
-			  // 
-			  this.btnLogin.Location = new System.Drawing.Point(77, 68);
-			  this.btnLogin.Name = "btnLogin";
-			  this.btnLogin.Size = new System.Drawing.Size(59, 25);
-			  this.btnLogin.TabIndex = 4;
-			  this.btnLogin.Text = "&Login";
-			  this.btnLogin.UseVisualStyleBackColor = true;
-			  this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
-			  // 
-			  // txtPassword
-			  // 
-			  this.txtPassword.Location = new System.Drawing.Point(77, 42);
-			  this.txtPassword.Name = "txtPassword";
-			  this.txtPassword.PasswordChar = '·';
-			  this.txtPassword.Size = new System.Drawing.Size(149, 20);
-			  this.txtPassword.TabIndex = 3;
-			  // 
-			  // LoginForm
-			  // 
-			  this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			  this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			  this.ClientSize = new System.Drawing.Size(235, 105);
-			  this.Controls.Add(this.btnLogin);
-			  this.Controls.Add(this.txtPassword);
-			  this.Controls.Add(this.txtUsername);
-			  this.Controls.Add(this.label2);
-			  this.Controls.Add(this.label1);
-			  this.Name = "LoginForm";
-			  this.Text = "Login";
-			  this.ResumeLayout(false);
-			  this.PerformLayout();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtUsername = new System.Windows.Forms.TextBox();
+			this.btnLogin = new System.Windows.Forms.Button();
+			this.txtPassword = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(58, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Username:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 46);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(56, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Password:";
+			// 
+			// txtUsername
+			// 
+			this.txtUsername.Location = new System.Drawing.Point(77, 13);
+			this.txtUsername.Name = "txtUsername";
+			this.txtUsername.Size = new System.Drawing.Size(149, 20);
+			this.txtUsername.TabIndex = 2;
+			// 
+			// btnLogin
+			// 
+			this.btnLogin.Location = new System.Drawing.Point(77, 68);
+			this.btnLogin.Name = "btnLogin";
+			this.btnLogin.Size = new System.Drawing.Size(59, 25);
+			this.btnLogin.TabIndex = 4;
+			this.btnLogin.Text = "&Login";
+			this.btnLogin.UseVisualStyleBackColor = true;
+			this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+			// 
+			// txtPassword
+			// 
+			this.txtPassword.Location = new System.Drawing.Point(77, 42);
+			this.txtPassword.Name = "txtPassword";
+			this.txtPassword.PasswordChar = '·';
+			this.txtPassword.Size = new System.Drawing.Size(149, 20);
+			this.txtPassword.TabIndex = 3;
+			// 
+			// LoginForm
+			// 
+			this.AcceptButton = this.btnLogin;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(235, 105);
+			this.Controls.Add(this.btnLogin);
+			this.Controls.Add(this.txtPassword);
+			this.Controls.Add(this.txtUsername);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "LoginForm";
+			this.Text = "Login";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
