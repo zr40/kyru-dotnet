7358e74ca8e421de29f4696bbe6065ba81838838
Steven Wokke <wokste@gmail.com>
2012-12-11 21:43:41 +0100

    Split system tray from program file

--- Kyru/Program.cs
+++ Kyru/Program.cs
@@ -1,26 +1,20 @@
 ﻿using System;
-using System.Drawing;
 using System.Windows.Forms;
 
 using Kyru.Core;
-using Kyru.Utilities;
+using Kyru;
 
 namespace Kyru
 {
 	internal static class Program
 	{
 		private static KyruApplication app;
-
-		private static NotifyIcon trayIcon;
-		private static ContextMenu trayMenu;
-
+		private static SystemTray tray;
 		[STAThread]
 		private static void Main()
 		{
 			Console.WriteLine("Kyru debug console");
 			Console.WriteLine();
-
-			KyruTimer.Start();
 
 			app = new KyruApplication();
 
@@ -29,50 +23,9 @@ namespace Kyru
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 
-			CreateSystemTray();
+			tray = new SystemTray(app);
 
 			Application.Run();
-
-			trayIcon.Visible = false;
-		}
-
-		private static void CreateSystemTray()
-		{
-			trayMenu = new ContextMenu();
-			trayMenu.MenuItems.Add("Log in", OnLogin);
-			trayMenu.MenuItems.Add("Add node", OnRegisterNode);
-			trayMenu.MenuItems.Add("-");
-			trayMenu.MenuItems.Add("System status", OnSystemStatus);
-			trayMenu.MenuItems.Add("-");
-			trayMenu.MenuItems.Add("Exit", OnExit);
-
-			trayIcon = new NotifyIcon();
-			trayIcon.Text = "Kyru";
-			trayIcon.Icon = new Icon("kyru.ico");
-			trayIcon.MouseDoubleClick += OnLogin;
-
-			trayIcon.ContextMenu = trayMenu;
-			trayIcon.Visible = true;
-		}
-
-		private static void OnExit(object sender, EventArgs e)
-		{
-			Application.Exit();
-		}
-
-		private static void OnLogin(object sender, EventArgs e)
-		{
-			new LoginForm(app.LocalObjectStorage).Show();
-		}
-
-		private static void OnRegisterNode(object sender, EventArgs e)
-		{
-			new AddNodeForm(app.Node.Kademlia).Show();
-		}
-
-		private static void OnSystemStatus(object sender, EventArgs e)
-		{
-			new SystemStatusForm(app).Show();
 		}
 	}
 }
---
+++ Kyru/SystemTray.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Drawing;
+using System.Windows.Forms;
+
+using Kyru.Core;
+using Kyru.Utilities;
+
+namespace Kyru
+{
+	internal class SystemTray
+	{
+		private KyruApplication app;
+
+		private NotifyIcon trayIcon;
+		private ContextMenu trayMenu;
+
+		internal SystemTray(KyruApplication app)
+		{
+			this.app = app;
+
+			trayMenu = new ContextMenu();
+			trayMenu.MenuItems.Add("Log in", OnLogin);
+			trayMenu.MenuItems.Add("Add node", OnRegisterNode);
+			trayMenu.MenuItems.Add("-");
+			trayMenu.MenuItems.Add("System status", OnSystemStatus);
+			trayMenu.MenuItems.Add("-");
+			trayMenu.MenuItems.Add("Exit", OnExit);
+
+			trayIcon = new NotifyIcon();
+			trayIcon.Text = "Kyru";
+			trayIcon.Icon = new Icon("Icons/kyru.ico");
+			trayIcon.MouseDoubleClick += OnLogin;
+
+			trayIcon.ContextMenu = trayMenu;
+			trayIcon.Visible = true;
+		}
+
+		private void OnExit(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void OnLogin(object sender, EventArgs e)
+		{
+			new LoginForm(app.LocalObjectStorage).Show();
+		}
+
+		private void OnRegisterNode(object sender, EventArgs e)
+		{
+			new AddNodeForm(app.Node.Kademlia).Show();
+		}
+
+		private void OnSystemStatus(object sender, EventArgs e)
+		{
+			new SystemStatusForm(app).Show();
+		}
+	}
+}
